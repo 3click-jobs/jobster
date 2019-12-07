d7a05be362c778af04913268f776672d990453b9
Goran Drakulić <drakulic@hotmail.com>
2019-12-07 15:09:19 +0100

    Create and get all company

--- client/src/ui/companies/browse/CompaniesContainer.js
+++ client/src/ui/companies/browse/CompaniesContainer.js
@@ -1,11 +1,58 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { actions as companiesActions } from '../../../redux/actions/companies'
+import { companiesSelectors } from '../../../redux/selectors/companies'
+import CompaniesTable from './CompaniesTable'
 
-export const CompaniesContainer = () => {
+export const CompaniesContainer = ({
+  companiesAll,
+  companiesIsLoading,
+  companiesIsError,
+  loadCompaniesAll
+}) => {
+
+  React.useEffect(() => {
+    loadCompaniesAll()
+  }, [loadCompaniesAll])
+
   return (
-    <div>
-      
-    </div>
+    <React.Fragment>
+      {
+        (!companiesIsLoading && companiesAll) &&
+        <React.Fragment>
+          <p>Number of companies in the database: {companiesAll.length}</p>
+          <CompaniesTable />
+        </React.Fragment>
+
+      }
+      {
+        (!companiesIsLoading && companiesIsError) &&
+        <p>Error at loading companies.</p>
+      }
+      {
+        companiesIsLoading &&
+        <p>Companies are loading ... </p>
+      }
+    </React.Fragment>
+
   )
 }
 
-export default CompaniesContainer
+const mapStateToProps = (state) => {
+  return {
+    companiesAll: companiesSelectors.getAll(state),
+    companiesIsLoading: companiesSelectors.isFetching(state),
+    companiesIsError: companiesSelectors.error(state)
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadCompaniesAll: () => dispatch(companiesActions.fetchAll())
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CompaniesContainer)
