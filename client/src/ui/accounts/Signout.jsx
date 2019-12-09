5c711619cb5c206c2e12935e4b188b87d86e1e9b
Goran Drakulić <drakulic@hotmail.com>
2019-12-09 19:23:53 +0100

    FE / Signout dialog

--- client/src/ui/accounts/Signout.jsx
+++ client/src/ui/accounts/Signout.jsx
@@ -1,22 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
 import { withRouter } from 'react-router-dom'
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 import { unassignCredentials } from '../../redux/actions/user'
 
 export const Signout = ({
   username,
-  role,
+  // role,
   unassignCredentials,
   history
 }) => {
+  const [open, setOpen] = useState(true);
+
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
+
+
+  // <div>
+  //     Sign out as {username}?
+  //     <button onClick={() => { unassignCredentials(); history.push('/') }}>SIGNOUT</button>
+  //     <button>Cancel</button>
+  // </div>
+
+
   return (
-    <div>
-      Sign out as {username}?
-      <button onClick={() => { unassignCredentials(); history.push('/') }}>SIGNOUT</button>
-      <button>Cancel</button>
-    </div>
+      <Dialog
+        open={open}
+        onClose={() => { setOpen(false); history.goBack() }}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Sign out"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+          Are you sure you want to sign out as {username}?.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => history.goBack() } color="primary">
+            Disagree
+          </Button>
+          <Button onClick={() => { unassignCredentials(); history.push('/') }} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
   )
 }
 
@@ -24,7 +60,7 @@ const mapStateToProps = (state) => ({
   // isFetching: accountsSelectors.isFetching(state),
   // isError: accountsSelectors.error(state),
   username: state.user.username,
-  role: state.user.accessRole
+  // role: state.user.accessRole
 })
 
 const mapDispatchToProps = (dispatch) => {
