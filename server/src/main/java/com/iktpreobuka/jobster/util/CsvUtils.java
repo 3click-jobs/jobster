7d9aeea99f1a3b6b52a4b4ffcfdc205c7215966e
Goran Drakulić <drakulic@hotmail.com>
2020-02-09 09:03:19 +0100

    BE / JobTypeController And JobTypeControllerTest

---
+++ server/src/main/java/com/iktpreobuka/jobster/util/CsvUtils.java
@@ -0,0 +1,20 @@
+package com.iktpreobuka.jobster.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+public class CsvUtils {
+	
+    private static final CsvMapper mapper = new CsvMapper();
+    
+    public static <T> List<T> read(Class<T> clazz, InputStream stream) throws IOException {
+        CsvSchema schema = mapper.schemaFor(clazz).withHeader().withColumnReordering(true);
+        ObjectReader reader = mapper.readerFor(clazz).with(schema);
+        return reader.<T>readValues(stream).readAll();
+    }
+}
