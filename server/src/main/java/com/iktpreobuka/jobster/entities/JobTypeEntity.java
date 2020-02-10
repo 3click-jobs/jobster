eb3385253ec0fe1ea7d709c89fa2a21fb80f2ea9
slobodif <slobodif@yahoo.com>
2020-02-10 10:33:15 +0100

    BE-second JobTypeEntity comented

--- server/src/main/java/com/iktpreobuka/jobster/entities/JobTypeEntity.java
+++ server/src/main/java/com/iktpreobuka/jobster/entities/JobTypeEntity.java
@@ -89,14 +89,14 @@ public class JobTypeEntity {
 		this.createdById = createdById;
 	}
 	
-	public JobTypeEntity(
-			@Pattern(regexp = "^[A-Za-z-, \\.\\/\\(\\)]{2,}$", message = "Job type name is not valid.") @NotNull(message = "Job type name must be provided.") String jobTypeName,
-			Integer createdById) {
-		super();
-		this.jobTypeName = jobTypeName;
-		this.status = getStatusActive();
-		this.createdById = createdById;
-	}
+//	public JobTypeEntity(
+//			@Pattern(regexp = "^[A-Za-z-, \\.\\/\\(\\)]{2,}$", message = "Job type name is not valid.") @NotNull(message = "Job type name must be provided.") String jobTypeName,
+//			Integer createdById) {
+//		super();
+//		this.jobTypeName = jobTypeName;
+//		this.status = getStatusActive();
+//		this.createdById = createdById;
+//	}
 
 
 	public List<JobSeekEntity> getSeeks() {
