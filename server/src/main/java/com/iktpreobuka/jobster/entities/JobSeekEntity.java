7541aeb8a8025c4cbf0f6f70036d8356ec6fdd01
slobodif <slobodif@yahoo.com>
2020-03-11 23:04:36 +0100

    BE-JobSeek & JobOffer POST fixed-begining, end, and created date compared

--- server/src/main/java/com/iktpreobuka/jobster/entities/JobSeekEntity.java
+++ server/src/main/java/com/iktpreobuka/jobster/entities/JobSeekEntity.java
@@ -1,6 +1,7 @@
 package com.iktpreobuka.jobster.entities;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -85,6 +86,11 @@ public class JobSeekEntity {
 	@NotNull (message = "End date must be provided.")
 	@Column(name="end_date")
 	private Date endDate;
+	
+	//@JsonView(Views.Student.class)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+	@Column(name="date_Created")
+	private Date dateCreated;
 	
 	@Column(name="flexibile_dates")
 	//@JsonView(Views.Admin.class)
@@ -317,6 +323,54 @@ public class JobSeekEntity {
 	}
 	
 	
+	
+	public JobSeekEntity(@NotNull(message = "Employee must be provided.") UserEntity employee,
+			@NotNull(message = "City must be provided.") CityEntity city,
+			@NotNull(message = "Job type must be provided.") JobTypeEntity type, List<JobDayHoursEntity> daysAndHours,
+			List<ApplyContactEntity> contacts, List<RejectSeekEntity> rejections, Integer id,
+			@NotNull(message = "Distance to job must be provided.") @Min(value = 0, message = "Distance to job must be {value} or higher!") Integer distanceToJob,
+			@NotNull(message = "Beginning date must be provided.") Date beginningDate,
+			@NotNull(message = "End date must be provided.") Date endDate, Date dateCreated, Boolean flexibileDates,
+			@NotNull(message = "Price must be provided.") @Min(value = 0, message = "Price must be {value} or higher!") Double price,
+			@NotNull(message = "Details must be provided.") String detailsLink, Boolean flexibileDays,
+			@NotNull(message = "Counter seek must be provided.") Boolean counterSeek, @Max(1) @Min(-1) Integer status,
+			Boolean expired, Integer createdById, Integer updatedById, Integer version) {
+		super();
+		this.employee = employee;
+		this.city = city;
+		this.type = type;
+		this.daysAndHours = daysAndHours;
+		this.contacts = contacts;
+		this.rejections = rejections;
+		this.id = id;
+		this.distanceToJob = distanceToJob;
+		this.beginningDate = beginningDate;
+		this.endDate = endDate;
+		this.dateCreated = Calendar.getInstance().getTime();
+		this.flexibileDates = flexibileDates;
+		this.price = price;
+		this.detailsLink = detailsLink;
+		this.flexibileDays = flexibileDays;
+		this.counterSeek = false;
+		this.status = getStatusActive();
+		this.expired = false;
+		this.createdById = createdById;
+		this.updatedById = updatedById;
+		this.version = version;
+	}	
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
 	public Boolean getCounterSeek() {
 		return counterSeek;
 	}
@@ -495,13 +549,6 @@ public class JobSeekEntity {
 
 	public void setRejections(List<RejectSeekEntity> rejections) {
 		this.rejections = rejections;
-	}
-
-
-
-	public void setExpired(boolean expired) {
-		// TODO Auto-generated method stub
-		
 	}
 	
 }
